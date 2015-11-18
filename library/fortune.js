ef614d6dab55c88643f72e531a37ecad73657b95
Enzo Matrix <alphanumeric@openmailbox.org>
2015-11-18 20:53:25 +0000

    functional fortunes told through node

--- library/fortune.js
+++ library/fortune.js
@@ -1,8 +1,7 @@
-var fortunes = ['you will score a goal', 'you will dream of code tonight', 'I am your father', 'beware the valanqar', 'you know nothing, jon snow'];
 
-function getFortune() {
-	var position = Math.floor(Math.random()*fortunes.length)
+exports.getFortune = function getFortune() {
+	var fortunes = ['you will score a goal', 'you will dream of code tonight', 'I am your father', 'beware the valanqar', 'you know nothing, jon snow'];
+	var position = Math.floor(Math.random()*fortunes.length);
 	return fortunes[position];
-	}
+	};
 
-exports.getFortunre = getFortune
